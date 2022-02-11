7205f65ce2bb7c4815e607a22754b2dadb52fe08
Yaseen AlMannaee <commit@yaseen.ae>
2022-02-11 23:07:42 +0400

    Removed sudo command from phusion image build. Added comments to scripts. Created docker-compose.yml

--- docker-src/run-nginx.sh
+++ docker-src/run-nginx.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# The below comments are preserved from the phusion/baseimage-docker repo for posterity
 # `/sbin/setuser nginx` runs the given command as the user `nginx`.
 # If you omit that part, the command will be run as root.
+
 exec nginx 

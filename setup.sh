6e800fc50fb48b52c54aa73941fbc52b2da100f3
Yaseen AlMannaee <commit@yaseen.ae>
2022-02-19 19:59:35 +0400

    Fix order of commands once again

--- setup.sh
+++ setup.sh
@@ -33,7 +33,6 @@ Option (1/2/3): "
 		else
 			echo "Set mdns name to rpileader.local"
 			host_name="rpileader"
-			return 0
 		fi
 		;;
 	2)
@@ -43,7 +42,7 @@ Option (1/2/3): "
 			else
 				echo "Set mdns name to rpifollower${i}.local"
 				host_name="rpifollower${i}"
-				return 0
+				break
 			fi
 		done
 		;;
@@ -55,6 +54,10 @@ Option (1/2/3): "
 		leader_or_follower
 		;;
 	esac
+	sudo bash -c "echo ${host_name} | tee /etc/hostname"
+	sudo bash -c "echo 127.0.1.1 ${host_name} >> /etc/hosts"
+	sudo bash -c "hostnamectl set-hostname ${host_name}"
+	sudo bash -c "systemctl restart avahi-daemon"
 }
 leader_or_follower
 
@@ -69,9 +72,7 @@ DuckDNS.org from a web browser and looking for the 'token:' field
 Entering these values constitutes accepting the LetsEncrypt TOS (https://letsencrypt.org/repository/) 
 "
 read -p "Enter the hostname (xxxxx.duckdns.org): " user_hostname
-echo "\n"
 read -p 'Enter the token (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx): ' user_duckdns_token
-echo "\n"
 read -p 'Enter the email SSL certificates will be issued for (xxxx@xxxx.com): ' user_email
 echo ""
 
@@ -80,14 +81,10 @@ if ! [ -x "$(command -v docker)" ]; then
 	sudo bash -c "$(curl -fsSL https://get.docker.com)"
 fi
 sudo bash -c "pip3 install --upgrade docker-compose"
-sudo bash -c "echo ${host_name} | tee /etc/hostname"
-sudo bash -c "echo 127.0.1.1 ${host_name} >> /etc/hosts"
-sudo bash -c "hostnamectl set-hostname ${host_name}"
-sudo bash -c "systemctl restart avahi-daemon"
 sudo bash -c "echo -e 'USER_HOSTNAME=${user_hostname}\nUSER_EMAIL=${user_email}\nUSER_DUCKDNS_TOKEN=${user_duckdns_token}' > ${PWD}/.env"
 sudo bash -c "mv --backup=t ${PWD}/docker-compose.yml ${PWD}/docker-compose-old.yml"
 sudo bash -c "curl \"https://raw.githubusercontent.com/y-almannaee/peltier-controller/main/docker-compose-default.yml\" > ${PWD}/docker-compose.yml"
 sudo bash -c "mkdir -p ${PWD}/app_data; chown -r pi:pi ${PWD}/app_data ${PWD}/docker-compose.yml ${PWD}/.env"
 sudo bash -c "/usr/local/bin/docker-compose pull"
-sudo bash -c "export DUCKDNS_TOKEN=${user_duckdns_token};/usr/bin/docker run goacme/lego --accept-tos --path ${PWD}/app_data/ --email ${user_email} --dns duckdns --domains ${user_hostname} --domains *.${user_hostname} run"
+sudo bash -c "export DUCKDNS_TOKEN=${user_duckdns_token};/usr/bin/docker run -e DUCKDNS_TOKEN goacme/lego --accept-tos --path ${PWD}/app_data/ --email ${user_email} --dns duckdns --domains ${user_hostname} --domains *.${user_hostname} run"
 sudo bash -c "echo '44 4 * * * root /usr/local/bin/docker-compose up letsencryptgo' > /etc/cron.d/lego-renew"

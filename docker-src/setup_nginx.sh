0643146c8d3be068a6fe261ee35c7275555a6e32
Yaseen AlMannaee <commit@yaseen.ae>
2022-02-26 23:26:35 +0400

    Add startup script and modified conf file

---
+++ docker-src/setup_nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sv stop nginx
+rm -r /etc/nginx/certs || :
+mkdir -p /etc/nginx/certs
+cp /var/lib/peltier_controller/certificates/*.key /etc/nginx/certs/certificate.key
+cp /var/lib/peltier_controller/certificates/*.issuer.crt /etc/nginx/certs/certificate.issuer.crt
+cp /var/lib/peltier_controller/certificates/*[^issuer].crt /etc/nginx/certs/certificate.crt
+cp /etc/nginx/default-site-conf/main_server /etc/nginx/sites-enabled
+sed -i -e 's@#INCLUDEOCSPSTAPLING@# OCSP stapling\
+	ssl_stapling on;\
+	ssl_stapling_verify on;\
+\
+	# verify chain of trust of OCSP response using Root CA and Intermediate certs\
+	ssl_trusted_certificate /etc/nginx/certs/certificate.issuer.crt;@' -e 's/#INCLUDESERVERNAME/server_name '"$HOSTNAME'";/' /etc/nginx/sites-enabled/main_server
+sv start nginx

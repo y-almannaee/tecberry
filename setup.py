d6f3ee149019891fd44b82ef2fd589fae0eaa3df
Yaseen AlMannaee <commit@yaseen.ae>
2022-02-26 16:24:12 +0400

    Created hostname code in Python

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # curl -fsSL https://raw.github.com/y-almannaee/peltier-controller/main/setup.py | sudo python3 -
 # on your Raspberry Pi!
 
-import shlex, subprocess, sys, traceback, os
+import shlex, subprocess, sys, traceback, os, time
 
 
 def pip_install(package):
@@ -15,7 +15,7 @@ def pip_install(package):
 	)
 
 
-def run_shell(cmd, success_state="", error_state=""):
+def run_shell(cmd, success_state="", error_state="", critical=False):
 	try:
 		subprocess.check_call(
 			shlex.split(cmd),
@@ -27,8 +27,49 @@ def run_shell(cmd, success_state="", error_state=""):
 	except subprocess.CalledProcessError as error:
 		print(error_state)
 		traceback.print_exception(error)
+		if critical:
+			print(f"{con_colors.WARNING}Critical failure has occurred, can't continue")
+			raise SystemExit()
 		return int(error.returncode)
 
+def set_hostname(hostname):
+	with open("/etc/hostname","w") as file:
+		file.write(hostname)
+	with open("/etc/hosts","a") as file:
+		file.write(f"127.0.1.1 {hostname}")
+	run_shell(f"hostnamectl set-hostname {hostname}")
+	run_shell(f"systemctl restart avahi-daemon")
+	time.sleep(5)
+
+def handle_hostname(choice):
+	run_shell("apt update",f"{con_colors.OKBLUE}Updated package lists")
+	run_shell("apt install -y avahi-utils",f"{con_colors.OKBLUE}Installed prerequisite programs")
+	import socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.settimeout(0)
+	try:
+		s.connect(('10.255.255.255', 1))
+		IP = s.getsockname()[0]
+	except Exception:
+		IP = '127.0.0.1'
+	finally:
+		s.close()
+	if choice is 1:
+		set_hostname("RaspberryPi_Leader")
+		out = subprocess.check_output(shlex.split(f"avahi-resolve -a {IP}")).decode().split('\t')[1].strip()
+		if out is not "RaspberryPi_Leader.local":
+			print(f"{con_colors.WARNING}This isn't the only RaspberryPi Leader on the network. Can't continue")
+			raise SystemExit()
+		else:
+			return
+	else:
+		for i in range(1,4):
+			set_hostname(f"RaspberryPi_Follower_{i}")
+			out = subprocess.check_output(shlex.split(f"avahi-resolve -a {IP}")).decode().split('\t')[1].strip()
+			if out is f"RaspberryPi_Follower_{i}.local":
+				return
+		print(f"{con_colors.WARNING}Too many Follower RaspberryPis on the network. Can't continue")
+		raise SystemExit()
 
 class con_colors:
 	HEADER = "\033[95m"
@@ -65,16 +106,14 @@ Option (a/b/c): """,
 while True:
 	choice = str(input()).lower()
 	if choice is "a":
-		host_name = "leader"
+		handle_hostname(1)
 	elif choice is "b":
-		host_name = "follower_1"
+		handle_hostname(2)
 	elif choice is "c":
 		break
 	else:
 		print("Enter a, b, or c")
 		continue
-	run_shell(f"hostnamectl set-hostname {host_name}")
-	run_shell(f"systemctl restart avahi-daemon")
 	break
 
 
@@ -97,7 +136,7 @@ user_email = input(
 	"Enter the email SSL certificates will be issued for (xxxx@xxxx.com): "
 )
 
-with open(f"{os.getcwd()}/.env",'r') as file:
+with open(f"{os.getcwd()}/.env",'w') as file:
 	file.writelines([f"USER_HOSTNAME={user_hostname}",f"USER_EMAIL={user_email}",f"USER_DUCKDNS_TOKEN={user_duckdns_token}"])
 
 run_shell("apt update",f"{con_colors.OKBLUE}Updated package lists")
@@ -112,5 +151,5 @@ run_shell(f'bash -c "mkdir -p {os.getcwd()}/app_data; chown -R pi:pi {os.getcwd(
 run_shell("/usr/local/bin/docker-compose pull")
 run_shell(f"/usr/bin/docker run -e DUCKDNS_TOKEN={user_duckdns_token} -v {os.getcwd()}/app_data:/var/lib goacme/lego --accept-tos --path /var/lib/ --email {user_email} --dns duckdns --domains {user_hostname} --domains *.{user_hostname} run")
 
-with open("/etc/cron.d/lego-renew",'r') as file:
+with open("/etc/cron.d/lego-renew",'w') as file:
 	file.writelines(["echo '44 4 * * * root /usr/local/bin/docker-compose up letsencryptgo"])

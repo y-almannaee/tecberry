dd4e00391e4c9ae6438cc384735b386dbab3fbef
Yaseen AlMannaee <commit@yaseen.ae>
2022-02-26 20:05:21 +0400

    Flesh out logic of setup.py

--- setup.py
+++ setup.py
@@ -15,20 +15,21 @@ def pip_install(package):
 	)
 
 
-def run_shell(cmd, success_state="", error_state="", critical=False):
+def run_shell(cmd, success_state="", error_state="", critical=False,shell=False):
+	cmd = shlex.split(cmd) if not shell else cmd
 	try:
 		subprocess.check_call(
-			shlex.split(cmd),
+			cmd,
 			stdout=subprocess.DEVNULL,
 			stderr=subprocess.DEVNULL,
+			shell=shell
 		)
 		print(success_state,con_colors.ENDC)
 		return 0
 	except subprocess.CalledProcessError as error:
 		print(error_state,con_colors.ENDC)
-		traceback.print_exception(error)
 		if critical:
-			print(f"{con_colors.WARNING}Critical failure has occurred, can't continue{con_colors.ENDC}")
+			print(f"{con_colors.FAIL}Critical failure has occurred, can't continue{con_colors.ENDC}")
 			raise SystemExit()
 		return int(error.returncode)
 
@@ -42,8 +43,8 @@ def set_hostname(hostname):
 	time.sleep(5)
 
 def handle_hostname(choice):
-	run_shell("apt update",f"{con_colors.OKBLUE}Updated package lists")
-	run_shell("apt install -y avahi-utils",f"{con_colors.OKBLUE}Installed prerequisite programs")
+	run_shell("apt update",f"{con_colors.OKCYAN}Updated package lists")
+	run_shell("apt install -y avahi-utils",f"{con_colors.OKCYAN}Installed prerequisite programs")
 	import socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.settimeout(0)
@@ -58,7 +59,7 @@ def handle_hostname(choice):
 		set_hostname("RaspberryPiLeader")
 		out = subprocess.check_output(shlex.split(f"avahi-resolve -a {IP}")).decode().split('\t')[1].strip()
 		if out != "RaspberryPiLeader.local":
-			print(f"{con_colors.WARNING}This isn't the only RaspberryPi Leader on the network. Can't continue{con_colors.ENDC}")
+			print(f"{con_colors.FAIL}This isn't the only RaspberryPi Leader on the network. Can't continue{con_colors.ENDC}")
 			raise SystemExit()
 		else:
 			return
@@ -68,7 +69,7 @@ def handle_hostname(choice):
 			out = subprocess.check_output(shlex.split(f"avahi-resolve -a {IP}")).decode().split('\t')[1].strip()
 			if out is f"RaspberryPiFollower{i}.local":
 				return
-		print(f"{con_colors.WARNING}Too many Follower RaspberryPis on the network. Can't continue{con_colors.ENDC}")
+		print(f"{con_colors.FAIL}Too many Follower RaspberryPis on the network. Can't continue{con_colors.ENDC}")
 		raise SystemExit()
 
 class con_colors:
@@ -91,16 +92,16 @@ print(
 	f"THIS ACTION MAY REMOVE PACKAGES, USE AT YOUR OWN RISK{con_colors.ENDC}\nPress Ctrl+C to close"
 )
 
-input("Press Enter to continue")
+input(f"{con_colors.BOLD}Press Enter to continue{con_colors.ENDC}")
 
 print(
 	f"""
-Is this Raspberry Pi a leader or follower?
+{con_colors.BOLD}Is this Raspberry Pi a leader or follower?{con_colors.ENDC}
 {con_colors.OKBLUE} a) {con_colors.ENDC}Leader
 {con_colors.OKBLUE} b) {con_colors.ENDC}Follower
 {con_colors.GRAY} c) Skip mDNS setup{con_colors.ENDC} 
 
-Option (a/b/c): """,
+{con_colors.BOLD}Option (a/b/c):{con_colors.ENDC} """,
 	end="",
 )
 while True:
@@ -112,37 +113,37 @@ while True:
 	elif choice == "c":
 		break
 	else:
-		print("Enter a, b, or c")
+		print(f"{con_colors.WARNING}Enter a, b, or c{con_colors.ENDC}")
 		continue
 	break
 
 
 print(
 	f"""
-A hostname is required for us to display the control information and camera feed on a website.
+{con_colors.BOLD}A hostname is required for us to display the control information and camera feed on a website.
 By default we use DuckDNS as it's a free and easy service. You may change this if you are technically savvy.
 
 A hostname may look like peltier.duckdns.org. You may register one at DuckDNS.org.
 A token will be of the form xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx, and you may find it by logging in to
 DuckDNS.org from a web browser and looking for the 'token:' field
 
-{con_colors.UNDERLINE}Entering these values constitutes the acceptance of LetsEncrypt TOS (https://letsencrypt.org/repository/){con_colors.ENDC}  
+{con_colors.UNDERLINE}{con_colors.WARNING}Entering these values constitutes the acceptance of LetsEncrypt TOS (https://letsencrypt.org/repository/){con_colors.ENDC}  
 """
 )
 
-user_hostname = input("Enter the hostname (xxxxx.duckdns.org): ")
-user_duckdns_token = input("Enter the token (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx): ")
+user_hostname = input(f"{con_colors.BOLD}Enter the hostname (xxxxx.duckdns.org):{con_colors.ENDC} ")
+user_duckdns_token = input(f"{con_colors.BOLD}Enter the token (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx):{con_colors.ENDC} ")
 user_email = input(
-	"Enter the email SSL certificates will be issued for (xxxx@xxxx.com): "
+	f"{con_colors.BOLD}Enter the email SSL certificates will be issued for (xxxx@xxxx.com):{con_colors.ENDC} "
 )
 
 with open(f"{os.getcwd()}/.env",'w') as file:
 	file.writelines([f"USER_HOSTNAME={user_hostname}",f"USER_EMAIL={user_email}",f"USER_DUCKDNS_TOKEN={user_duckdns_token}"])
 
-run_shell("apt update",f"{con_colors.OKBLUE}Updated package lists")
-run_shell("apt full-upgrade -y",f"{con_colors.OKBLUE}Upgraded system")
-run_shell("apt install -y avahi-utils avahi-daemon git python3 python3-pip cron",f"{con_colors.OKBLUE}Installed prerequisite programs")
-if run_shell("command -v docker",f"{con_colors.OKBLUE}Installing Docker",f"{con_colors.OKBLUE}Docker is already installed, skipping") == 1:
+run_shell("apt update",f"{con_colors.OKCYAN}Updated package lists")
+run_shell("apt full-upgrade -y",f"{con_colors.OKCYAN}Upgraded system")
+run_shell("apt install -y avahi-utils avahi-daemon git python3 python3-pip cron",f"{con_colors.OKCYAN}Installed prerequisite programs",)
+if run_shell("command -v docker",f"{con_colors.OKCYAN}Docker is already installed, skipping",f"{con_colors.OKCYAN}Installing Docker",True,True) != 0:
 	run_shell("bash -c \"$(curl -fsSL https://get.docker.com)\"")
 pip_install("docker-compose")
 run_shell(f"mv --backup=t {os.getcwd()}/docker-compose.yml {os.getcwd()}/docker-compose-old.yml")

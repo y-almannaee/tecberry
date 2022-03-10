9d1cfda4d4a4803becdfff3513b9f9564e800b38
Yaseen AlMannaee <commit@yaseen.ae>
2022-03-10 19:12:14 +0400

    Move sources to this branch, because gh-pages gets overwritten :)

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,6 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/y-almannaee/peltier-controller.git', // Update to point to your repository  
 		user: {
 			name: 'Yaseen AlMannaee', // update to use your name
 			email: 'commit@yaseen.ae' // Update to use your email
